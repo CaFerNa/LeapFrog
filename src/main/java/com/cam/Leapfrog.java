7e18c6fc9d49798703ab6335d7e201abe8cdc481
Camilo <camilo.f@csic.es>
2026-02-04 21:20:46 +0100

    Commit

--- src/main/java/com/cam/Leapfrog.java
+++ src/main/java/com/cam/Leapfrog.java
@@ -7,9 +7,10 @@ import static com.cam.TableroPiezas.renderTablero;
 public class Leapfrog {
 
     public static String[] Tablero_Inicio = {"X","X","X","X"," ","O","O","O","O"};
-    public static String[] tablero = {"O","O","O","O"," ","X","X","X","X"};
+    public static String[] tablero = {"X","X","X","X"," ","O","O","O","O"};
     public static int contador;
     public static boolean playGame;
+    public static int[] firstMove = new int[2];
     // OP_CODES
     public static final int OP_GENERAR_TABLERO = 1;
     public static final int OP_MOVER_PIEZA = 2;
@@ -25,14 +26,20 @@ public class Leapfrog {
     }
 
     @Export(name = "runFunction")
-    public static String runFunction(int OP_CODE, String id) {
+    public static String runFunction(int OP_CODE, int id) {
         switch (OP_CODE){
-            case OP_GENERAR_TABLERO: return TableroPiezas.renderTablero(Tablero_Inicio);
-            case OP_MOVER_PIEZA:
-                comprobarTablero();
-                return TableroPiezas.renderTablero(tablero);
+            case OP_GENERAR_TABLERO: return TableroPiezas.renderTablero(Tablero_Inicio, 10);
+            case OP_MOVER_PIEZA: return gameLoop(id);
             default: return "<span style='color:red'>OP_CODE no válido</span>";
         }
+    }
+
+    public static void inicializar() {
+        // tablero = Tablero_Inicio;
+        contador = 0;
+        playGame = true;
+        for (int i = 0; i < firstMove.length; i++)  firstMove[i] = 0;
+        //System.out.println("Juego Inicializado");
     }
 
     public static void comprobarTablero() {
@@ -43,16 +50,38 @@ public class Leapfrog {
         if (correctos == Tablero_Inicio.length) {
             renderHtml("<span style='color:red'>¡Has Ganado!</span>");
             System.out.println("Has Ganado!");
+            playGame = false;
         } else {
             System.out.println("No has Ganado. Te faltan " + ((Tablero_Inicio.length)-correctos) + " para ganar.");
+            playGame = true;
         }
 
     }
 
-    public static void inicializar() {
-        // tablero = Tablero_Inicio;
-        contador = 0;
-        playGame = true;
-        //System.out.println("Juego Inicializado");
+    public static String moverPiezas(int cod_pieza) {
+        if (firstMove[0] == 0) {
+            firstMove[0] = 1;
+            firstMove[1] = cod_pieza;
+            System.out.println(firstMove[1]);
+            return "Primer click";
+        } else{
+            firstMove[0] = 0;
+            tablero[cod_pieza] = tablero[firstMove[1]];
+            System.out.println(tablero[cod_pieza]);
+            return "Segundo click";
+        }
+    }
+
+    public static void endGame() {
+
+    }
+
+    public static String gameLoop(int id) {
+        comprobarTablero();
+        if (playGame) {
+            moverPiezas(id);
+            return TableroPiezas.renderTablero(tablero, id);
+        } else return TableroPiezas.renderTablero(tablero, 10);
+
     }
 }
--- src/main/java/com/cam/TableroPiezas.java
+++ src/main/java/com/cam/TableroPiezas.java
@@ -4,18 +4,23 @@ import java.util.Arrays;
 
 public class TableroPiezas {
 
-    public static String renderTablero(String[] piezas) {
+    public static String renderTablero(String[] piezas, int piezaPulsada) {
         StringBuilder sb = new StringBuilder();
         String pieza = "";
+        String color = "";
         sb.append("<div style='display: flex; justify-content: space-between; align-items: center; width: 100%;'>");
         for (int i = 0; i < piezas.length ; i++) {
             pieza = (piezas[i]);
             if (pieza.equals("O")) {
-                sb.append("<button type='button' id='" + i + "' style='width: 10%; aspect-ratio: 2/1; background-color: #3b82f6; color: white; border: none; border-radius: 4px; cursor: pointer; box-sizing: border-box; font-size: larger;'>O</button>");
+                if (i == piezaPulsada) {color = "#898989";} else {color = "#3b82f6";}
+                sb.append("<button type='button' id='" + i + "' style='width: 10%; aspect-ratio: 2/1; background-color: "+color+"; color: white; border: none; border-radius: 4px; cursor: pointer; box-sizing: border-box; font-size: larger;'>O</button>");
             } else if (pieza.equals("X")) {
-                sb.append("<button type='button' id='" + i + "'style='width: 10%; aspect-ratio: 2/1; background-color: #ff0000; color: white; border: none; border-radius: 4px; cursor: pointer; box-sizing: border-box; font-size: larger;'>X</button>");
-            } else
-                sb.append("<button type='button' id='" + i + "'style='width: 10%; aspect-ratio: 2/1; background-color: #ffffff; color: white; border: 1px solid #eee; border-radius: 4px; cursor: pointer; box-sizing: border-box;'>&nbsp;</button>");
+                if (i == piezaPulsada) {color = "#898989";} else {color = "#ff0000";}
+                sb.append("<button type='button' id='" + i + "'style='width: 10%; aspect-ratio: 2/1; background-color: "+color+"; color: white; border: none; border-radius: 4px; cursor: pointer; box-sizing: border-box; font-size: larger;'>X</button>");
+            } else {
+                color = "white";
+                sb.append("<button type='button' id='" + i + "'style='width: 10%; aspect-ratio: 2/1; background-color: " + color + "; color: white; border: 1px solid #eee; border-radius: 4px; cursor: pointer; box-sizing: border-box;'>&nbsp;</button>");
+            }
         }
         sb.append("</div>");
         return sb.toString();
